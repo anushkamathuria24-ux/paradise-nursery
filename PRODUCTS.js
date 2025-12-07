b3034e691879aae3439cad76d92370db774ca073
Anushka Mathuria <anushkamathuria24@gmail.com>
2025-12-08 01:18:26 +0530

    Add files via upload

---
+++ PRODUCTS.js
@@ -0,0 +1,58 @@
+/*
+===============================================================
+               PARADISE NURSERY SHOPPING APP
+===============================================================
+*/
+// -------------------- PRODUCT IMAGE DATA --------------------
+export const PRODUCTS = [
+    {
+        id: "ficus",
+        name: "Ficus Bonsai",
+        category: "Indoor Trees",
+        price: 899.0,
+        image: "", https:  //hips.hearstapps.com/hmg-prod/images/ficus-tree-2-64499256b4573.jpg"",
+    },
+    {
+        id: "monstera",
+        name: "Monstera Deliciosa",
+        category: "Tropical",
+        price: 1299.0,
+        image: "", https:  //tse1.mm.bing.net/th/id/OIP.k8M_hu93EGCNBLJABsA3PgHaHa?pid=Api&P=0&h=180"",
+    },
+    {
+        id: "snake",
+        name: "Snake Plant",
+        category: "Easy Care",
+        price: 499.0,
+        image: "", https:  //tse1.mm.bing.net/th/id/OIP.5CXqdfWyNtuX3IA-I5AdcQHaIw?pid=Api&P=0&h=180"",
+    },
+
+    {
+        id: "pothos",
+        name: "Golden Pothos",
+        category: "Vines",
+        price: 349.0,
+        image: "", https:  //www.thespruce.com/thmb/iBuFkdw4jum43xKwa0OYS6PfuO8=/4000x0/filters:no_upscale():max_bytes(150000):strip_icc()/hawaiian-pothos-growing-guide-7092450-hero-A-cd0d4bba36284002a7dd3be7ef419cf9.jpg"",
+    },
+    {
+        id: "succulent",
+        name: "Succulent Mix",
+        category: "Succulents",
+        price: 299.0,
+        image: "", https:  //www.flowerglossary.com/wp-content/uploads/2019/07/crassula-ovata-768x768.png"",
+    },
+    {
+        id: "calathea",
+        name: "Calathea Orbifolia",
+        category: "Tropical",
+        price: 799.0,
+        image: "", https:  //www.thespruce.com/thmb/T2UsvAuNq8-LImXOjDufvO31JHY=/4000x0/filters:no_upscale():max_bytes(150000):strip_icc()/SPR-calathea-zebrina-plant-profile-7374039-Hero-A-e81921d144714d6c9ccdf001cf4a2df8.jpg"",
+    },
+    {
+        id: "zz",
+        name: "ZZ Plant",
+        category: "Easy Care",
+        price: 599.0,
+        image: "", https:  //tse1.mm.bing.net/th/id/OIP.-2pBV0DcZgjTFGPJ3TzZhAHaE8?pid=Api&P=0&h=180"",
+    },
+];
